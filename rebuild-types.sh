4e61e9d8d05a318ca23907b3ac1fd9a79c26aa29
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-10-01 12:05:36 +0100

    Newest qdb

--- rebuild-types.sh
+++ rebuild-types.sh
@@ -7,13 +7,20 @@ translate() {
 	iconv -f utf-8 -t ascii//TRANSLIT | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9 ]//g' | sed 's/ /_/g'
 }
 
-rm types.db assoc.db
-$qdb -l index.db:s | while read link flags name; do
+rm -rf /tmp/types/* types.db assoc.db 2>/dev/null || true
+mkdir /tmp/types 2>/dev/null || true
+
+$qdb -l index.db | while read link flags name; do
 	test -f $link/type || continue
 	cat $link/type | while read type; do
 		test ! -z "$type" || continue;
 		typeid="`echo $type | translate`"
-		$qdb -p "$typeid:$type" types.db:s:s
-		echo "-p\"$link:$typeid\""
-	done | xargs -I {} $qdb {} assoc.db:2s >/dev/null
+ 		$qdb -p "$typeid:$type" types.db >/dev/null
+		echo $link >> /tmp/types/$typeid
+	done
+done
+
+ls /tmp/types | while read type; do
+	songs="`cat /tmp/types/$type | tr '\n' ' '`"
+	$qdb -p"$type:$songs" assoc.db >/dev/null
 done
